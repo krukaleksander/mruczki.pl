5fdc578890e7b101323d97a374fa6e5bb5496b20
Alek Kruk <kruk.aleksander@hotmail.com>
2020-01-10 07:17:45 +0100

    404 ready. about us in process

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php get_header() ?>
+<div class="wrap-404">
+    <h2 class="heading-404">Ups...</h2>
+    <p class="desc-404">Mrrrau, to dobry adres?</p>
+    <div class="img-404-cont">
+        <img class="img-fluid img-404" src="<?php bloginfo('template_url'); ?>/img/lili.jpg" alt="zdjęcie Lili">
+    </div>    
+    
+    
+</div>
+<?php get_footer() ?>
