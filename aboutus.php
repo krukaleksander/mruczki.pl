c680b0dfba38999ab57f48354eacd47b39db9a78
Alek Kruk <kruk.aleksander@hotmail.com>
2020-01-10 07:57:55 +0100

    about us ready, need to consult with owner

--- aboutus.php
+++ aboutus.php
@@ -1,3 +1,17 @@
+<?php 
+/*
+Template Name: O nas
+*/
+?>
 <?php get_header() ?>
-
+<div class="wrap-about">
+<h2 class="heading-about">O nas</h2>
+<p class="telephone"><i class="fas fa-mobile-alt"></i> 535963521</p>
+<p class="desc-about"><strong>W</strong> moim życiu koty pojawiły się stosunkowo nie dawno, w domu rodzinnym od zawsze mieszkała z nami suczka Balbina, która nie była miłośniczką mruczków. Choć marzyłam o kocie, wiedziałam, że może być źle przyjęty a życie w stresie i ciągłe ucieczki przed psem to nie życie dla kota.. </p>
+<p class="desc-about"><strong>K</strong>iedy moje marzenie o adopcji kota mogło się spełnić, jeszcze nie wiedziałam, że ta decyzja tak bardzo wpłynie na moje życie.. Choć zawsze myślałam o czarnym kocie, trafiłam pewnego dnia na ogłoszenie Lili - niedobrego kociego jedynaka, który pilnie szukał domu.. no i klamka zapadła ;) Lili pojawiła się w naszym domu i zmieniła wszystko o 180 stopni. Nie była otwartym przytulaskiem, przed dwa tygodnie mieszkała pod kanapą a my wychodziliśmy z siebie starając się aby poczuła się bezpieczna.. </p>
+<p class="desc-about"><strong>P</strong>iotrusia, naszego wymarzonego czarnuszka znaleźliśmy zimą w lesie.. Burszuras został podrzucony do stada kotów wolnożyjących, a Tadzio mieszkał na wsi w stadzie 40 porzuconych kotów.. oprócz kociej przyjaźni łączy ich jeszcze jedno - mają swojego psa.</p>
+<p class="desc-about"><strong>D</strong>om tymczasowy który stworzyłam na przestrzeni lat przyjął wiele kotów, znalazły one fantastycznych opiekunów.. choć niektóre wydawały się “nieadpcyjne” - dzikuski, chore, starsze - dostały swoje szanse.. Współpracuje z Fundacją Znajdki, wszystkich zachęcam do adopcji i wsparcia zwierzaków, które są pod opieką Znajdek - <a class="link-znajdki" href="http://znajdki.pl" rel="noopener noreferrer" target="_blank">znajdki.pl</a></p>
+<p class="desc-about"><strong>A</strong>dopcja kociego przyjaciela to ważna i odpowiedzialna decyzja, osobom które decydują się na adopcje moich tymczasów oferuje pełne wsparcie, pomóc w czasie ale i również po adopcji.. </p>
+<h2 class="heading-about">Zapraszam do mojego kociego świata!</h2>
+</div>
 <?php get_footer() ?>
